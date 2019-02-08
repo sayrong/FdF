50e312355839e578b2d89f01e3bcd73add8258a0
Babette Alvyn sharp <balvyn-s@e4r9p1.21-school.ru>
2019-02-08 21:02:16 +0300

    keys for mac

--- keys.h
+++ keys.h
@@ -1,6 +1,7 @@
 #ifndef KEYB_LINUX_H
 # define KEYB_LINUX_H
 // вместо linux key
+
 # define KB_ESC						0xff1b
 # define KB_UP						65362	//126
 # define KB_DOWN					65364	//125
@@ -17,5 +18,26 @@
 # define KB_H						104
 # define KB_X						120
 # define KB_Z						122
+
+
+/*
+//Mac
+# define KB_ESC						53
+# define KB_UP						125
+# define KB_DOWN					126
+# define KB_LEFT					124
+# define KB_RIGHT					123
+# define KP_4						1 // s
+# define KP_6						0 // a
+# define KP_8						13 //w
+# define KP_2						12 //q
+# define KB_I						6 //z
+# define KB_U						7 // x
+# define KB_W						24 // +
+# define KB_S						27 // -
+# define KB_H						18 // 1
+# define KB_X						19 // 2
+# define KB_Z						20 // 3
+*/
 
 # endif
